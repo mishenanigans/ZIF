c847fc99039ee729f8bbf6c94b58ea7b5c761d2c
Misia <bloniarz@users.noreply.github.com>
2018-11-23 00:40:50 -0700

    oh god copied code
    aaaaaaaaaaaa

---
+++ Assets/Player_Move_Prot.cs
@@ -0,0 +1,55 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Player_Move_Prot : MonoBehaviour
+{
+
+    float moveX;
+    public int playerspeed = 50;
+    private bool facingRight = false;
+    public int playerJumpPower = 1250;
+
+    // Use this for initialization
+    void Start()
+    {
+
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        //CONTROLS
+        moveX = Input.GetAxis("Horizontal");
+        if (Input.GetButtonDown("Jump"))
+        {
+            Jump();
+        }
+        //ANIMATIONS
+        //PLAYER DIRECTION
+        if (moveX < 0.0f && facingRight == false)
+        {
+            FlipPlayer();
+        }
+        else if (moveX > 0.0f && facingRight == true)
+        {
+            FlipPlayer();
+        }
+        //PHYSICS
+        gameObject.GetComponent<Rigidbody2D>().velocity = new Vector2(moveX * playerspeed, gameObject.GetComponent<Rigidbody2D>().velocity.y);
+    }
+
+    void Jump()
+    {
+        //JUMPING COD
+        GetComponent<Rigidbody2D>().AddForce(Vector2.up * playerJumpPower);
+    }
+
+    void FlipPlayer()
+    {
+        facingRight = !facingRight;
+        Vector2 localScale = gameObject.transform.localScale;
+        localScale.x *= -1;
+        transform.localScale = localScale;
+    }
+}
